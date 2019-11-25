53b51330088f752a9209866d06a7d172358da711
Mikhail Kilin <1kilinmv1@gmail.com>
2019-11-25 15:29:05 +0300

    Started Work on menu of client

---
+++ ChessClient/src/Main.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("menu.fxml"));
+        primaryStage.setTitle("Chess");
+        primaryStage.setScene(new Scene(root, 880, 880));
+        primaryStage.show();
+        primaryStage.setResizable(false);
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
